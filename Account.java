27b1984900b29027379f55febbbad5f99ef2fd9b
ramizarif <72959302+ramizarif@users.noreply.github.com>
2021-05-03 13:59:53 -0400

    Updated Account.java
    Has functionality for making accounts public and private. Also has a method that checks to see if an account is public or private.

--- Account.java
+++ Account.java
@@ -1,4 +1,12 @@
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.nio.Buffer;; 
 
 public class Account {
 
@@ -114,14 +122,63 @@ public class Account {
 
     public boolean isFriend(Account o) {
         for (int i = 0; i < friendsList.size(); i++) {
-            if (o.getUsername() == friendsList.get(i)) {
+            if (o.getUsername().equals(friendsList.get(i))) {
+                System.out.println(o.getUsername() + "and" + friendsList.get(i));
                 return true; 
             } 
         }
-        for (int k = 0; k < o.getFriendsList().size(); k++) {
-            if (username == o.getFriendsList().get(k)); 
-            return true; 
-        }
         return false; 
     }
+    public void makePrivate(String name) throws FileNotFoundException {
+        File f = new File("privateaccs.txt");
+        FileOutputStream fos = new FileOutputStream(f,true); 
+        PrintWriter pw = new PrintWriter(fos);  
+
+        pw.println(name);
+        pw.close();
+        
+    }
+
+    public void makePublic(String name) throws FileNotFoundException, IOException {
+        File f = new File("privateaccs.txt");
+        File f2 = new File("tempPriv.txt");
+        FileReader fr = new FileReader(f);
+        BufferedReader bfr = new BufferedReader(fr);
+        FileOutputStream fos = new FileOutputStream(f2,true); 
+        PrintWriter pw = new PrintWriter(fos);
+
+        String readLine = bfr.readLine();
+        while(readLine != null) {
+            if (readLine.equals(name)) {
+
+            } else {
+                pw.println(readLine);
+            }
+            
+            readLine = bfr.readLine();
+        }
+        
+        bfr.close();
+        pw.close();
+        boolean y = f.delete();
+        boolean x = f2.renameTo(new File("privateaccs.txt"));
+    }
+
+    public boolean isPrivate(Account a) throws IOException {
+        File f = new File("privateaccs.txt");
+        FileReader fr = new FileReader(f);
+        BufferedReader bfr = new BufferedReader(fr);
+        String readLine = bfr.readLine();
+        while(readLine != null) {
+            if (readLine.equals(a.getUsername())) {
+                return true; 
+            }
+            readLine = bfr.readLine();
+        }
+
+        return false; 
+        
+
+    }
+
 }
